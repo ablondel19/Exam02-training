a503fb7e6b1bb62e69677dab57950a47d61f6e47
Arnaud Blondel <ablondel@fu-r10-p5.s19.be>
2022-01-06 15:30:55 +0100

    gnl training

---
+++ gnl.c
@@ -0,0 +1,175 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gnl.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablondel <ablondel@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/05 14:53:06 by ablondel          #+#    #+#             */
+/*   Updated: 2022/01/06 15:24:09 by ablondel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+int k = 1;
+
+int		ft_len(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int		ft_nl(char *s)
+{
+	int i = 0;
+	while (s[i])
+	{
+		if (s[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+char 	*ft_extract(char *src, int nl)
+{
+	int i = 0;
+	char *dst = NULL;
+	dst = malloc(sizeof(char) * (ft_len(src) + 1));
+	if (!dst)
+		return (NULL);
+	while (src[i] && i <= nl)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_update(char *src, int nl)
+{
+	int i = 0;
+	char *dst = NULL;
+	dst = malloc(sizeof(char) * (ft_len(src) + 1));
+	if (!dst)
+		return (NULL);
+	while (src[nl])
+	{
+		dst[i] = src[nl];
+		nl++;
+		i++;
+	}
+	dst[i] = '\0';
+	free(src);
+	src = dst;
+	return (src);
+}
+
+char	*ft_strdup(char *src)
+{
+	char *dst = NULL;
+	int i = -1;
+
+	dst = malloc(sizeof(char) * (ft_len(src) + 1));
+	if (!dst)
+		return (NULL);
+	while (src[++i])
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int i = 0;
+	char *dst = NULL;
+
+	dst = malloc(sizeof(char) * (ft_len(s1) + ft_len(s2) + 1));
+	if (!dst)
+		return (NULL);
+	while (*s1)
+		dst[i++] = *s1++;
+	while (*s2)
+		dst[i++] = *s2++;
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*remaining;
+	int			ret;
+	char		*line;
+	char		*tmp;
+	char		buf[BUFFER_SIZE + 1];
+
+	if (BUFFER_SIZE < 1 || fd < 0)
+		return (NULL);
+	if (!remaining)
+		if (!(remaining = ft_strdup("")))
+			return (NULL);
+	ret = BUFFER_SIZE;
+	if (k == -1)
+		return (NULL);
+	while (1)
+	{
+		while (ret == BUFFER_SIZE && ft_nl(buf) == -1)
+		{
+			ret = read(fd, buf, BUFFER_SIZE);
+			if (ret == -1)
+				return (NULL);
+			buf[ret] = '\0';
+			tmp = remaining;
+			remaining = ft_strjoin(remaining, buf);
+			free(tmp);
+			if (!remaining)
+				return (NULL);
+		}
+		if (ft_nl(remaining) >= 0)
+		{
+			line = ft_extract(remaining, ft_nl(remaining));
+			if (!line)
+				return (NULL);
+			tmp = remaining;
+			remaining = ft_update(remaining, ft_nl(remaining) + 1);
+			if (!remaining)
+				return (NULL);
+			return (line);
+		}
+		if (ft_nl(remaining) < 0)
+		{
+			k = -1;
+			if (!(line = ft_strdup(remaining)))
+				return (NULL);
+			free(remaining);
+			return (line);
+		}
+	}
+	return (NULL);
+}
+
+int		main(void)
+{
+	int fd = open("test", O_RDONLY);
+	char *s = NULL;
+	while (1)
+	{
+		s = get_next_line(fd);
+		if (!s)
+		{
+			printf("%s", s);
+			system("leaks a.out");
+			return (0);
+		}
+		printf("%s", s);
+		free(s);
+	}
+}
